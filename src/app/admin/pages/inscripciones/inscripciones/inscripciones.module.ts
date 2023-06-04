bd3b606a031bc6bec416e071fdcd47e1c9d0a7d9
Juliana Quintero <juli.carmesi@gmail.com>
2023-06-04 18:45:47 -0500

    css changes

--- src/app/admin/pages/inscripciones/inscripciones/inscripciones.module.ts
+++ src/app/admin/pages/inscripciones/inscripciones/inscripciones.module.ts
@@ -13,6 +13,7 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { ReactiveFormsModule } from "@angular/forms";
 import { MatInputModule } from "@angular/material/input";
 import { MatSelectModule } from "@angular/material/select";
+import { PipesModule } from "../../../../share/pipes/pipes.module";
 
 @NgModule({
   declarations: [InscripcionesComponent, InscripcionDialogComponent],
@@ -27,6 +28,7 @@ import { MatSelectModule } from "@angular/material/select";
     ReactiveFormsModule,
     MatInputModule,
     MatSelectModule,
+    PipesModule,
   ],
   exports: [InscripcionesComponent],
 })
